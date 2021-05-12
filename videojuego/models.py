f7ae27388ed292c788608fb25043d3d720565389
Gerardo Jimenez Arguelles <33142689@uaz.edu.mx>
2021-05-11 22:08:08 -0500

    Correccion botones disabled.

--- videojuego/models.py
+++ videojuego/models.py
@@ -8,7 +8,7 @@ class Videojuego(models.Model):
     titulo = models.CharField('Titulo', max_length=50, unique=True)
     anio = models.IntegerField('Año')
     categoria = models.ForeignKey("videojuego.Categoria", verbose_name='categoria', on_delete=models.CASCADE)
-    precio = models.DecimalField(max_digits=5, decimal_places=2)
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
     stock = models.IntegerField('Stock')
     descripcion = models.CharField('Descripción',max_length=250, null=True, blank=True) 
 
@@ -21,15 +21,15 @@ class Categoria(models.Model):
     def __str__(self):
         return self.nombre
 
-# class Venta(models.Model):
-#     fecha = models.DateField()
-#     usuario = models.ForeignKey("usuarios.Usuario", verbose_name='Usuario', on_delete=models.CASCADE)
-#     total = models.DecimalField(max_digits=8, decimal_places=2)
+class Venta(models.Model):
+    fecha = models.DateField(auto_now_add=True)
+    usuario = models.ForeignKey("usuarios.Usuario", verbose_name='Usuario', on_delete=models.CASCADE)
+    total = models.DecimalField(max_digits=8, decimal_places=2)
 
-# class DetalleVenta(models.Model):
-#     venta = models.ForeignKey("usuarios.Venta", verbose_name='Venta', on_delete=models.CASCADE)
-#     id_videojuego = models.CharField('Titulo', max_length=50, unique=True)
-#     titulo_videojuego = models.CharField('Titulo', max_length=50, unique=True)
-#     precio = models.DecimalField(max_digits=5, decimal_places=2)
-#     cantidad = models.IntegerField('cantidad')
-#     total = models.DecimalField(max_digits=7, decimal_places=2)
+class DetalleVenta(models.Model):
+    venta = models.ForeignKey("videojuego.Venta", verbose_name='Venta', on_delete=models.CASCADE)
+    id_videojuego = models.CharField('Titulo', max_length=50, unique=True)
+    titulo_videojuego = models.CharField('Titulo', max_length=50, unique=True)
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+    cantidad = models.IntegerField('cantidad')
+    total = models.DecimalField(max_digits=7, decimal_places=2)
